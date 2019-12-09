6a5cb12d36c99dbb5df1e6c1a1cbf3f3d46e71de
Ho Minh Duong <minhans29@gmail.com>
2019-12-09 11:18:29 +0700

    create Home Tab and Search Tab

--- app/src/main/java/com/example/zingmp3/Activity/MainActivity.java
+++ app/src/main/java/com/example/zingmp3/Activity/MainActivity.java
@@ -1,16 +1,44 @@
 package com.example.zingmp3.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.zingmp3.Adapter.MainViewPagerAdapter;
+import com.example.zingmp3.Fragment.Fragment_TimKiem;
+import com.example.zingmp3.Fragment.Fragment_TrangChu;
 import com.example.zingmp3.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        anhxa();
+        init();
+        
+    }
+
+    private void init() {
+        MainViewPagerAdapter mainViewPagerAdapter= new MainViewPagerAdapter(getSupportFragmentManager());
+        mainViewPagerAdapter.addFragment(new Fragment_TrangChu(), "Home Page");
+        mainViewPagerAdapter.addFragment(new Fragment_TimKiem(), "Search Page");
+        viewPager.setAdapter(mainViewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.icontrangchu);
+        tabLayout.getTabAt(1).setIcon(R.drawable.iconsearch);
+
+    }
+
+    private void anhxa() {
+        tabLayout= findViewById(R.id.mytabLayout);
+        viewPager= findViewById(R.id.myViewPager);
+
     }
 }
--- app/src/main/java/com/example/zingmp3/Adapter/MainViewPagerAdapter.java
+++ app/src/main/java/com/example/zingmp3/Adapter/MainViewPagerAdapter.java
@@ -1,4 +1,38 @@
 package com.example.zingmp3.Adapter;
 
-public class MainViewPagerAdapter {
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class MainViewPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> arrayFragmemt=new ArrayList<>();//dung de add vao cac fragmemt
+    private ArrayList<String> arrayTitle = new ArrayList<>();  //dung de luu title cua moi fragmemt o tren
+    public MainViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return arrayFragmemt.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return arrayFragmemt.size();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        arrayFragmemt.add(fragment);
+        arrayTitle.add(title);
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return arrayTitle.get(position);
+    }
 }
