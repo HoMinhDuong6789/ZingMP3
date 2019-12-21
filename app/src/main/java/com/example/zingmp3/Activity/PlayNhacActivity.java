f37629424e3b19d08acd5479af8ef7f456fcb90e
Ho Minh Duong <minhans29@gmail.com>
2019-12-21 09:49:36 +0700

    Play Song 59

--- app/src/main/java/com/example/zingmp3/Activity/PlayNhacActivity.java
+++ app/src/main/java/com/example/zingmp3/Activity/PlayNhacActivity.java
@@ -6,17 +6,30 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.example.zingmp3.Adapter.ViewPagerPlaylistnhac;
+import com.example.zingmp3.Fragment.Fragment_Dianhac;
+import com.example.zingmp3.Fragment.Fragment_playdanhsachcacbaihat;
 import com.example.zingmp3.Model.BaiHat;
 import com.example.zingmp3.R;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.logging.SimpleFormatter;
 
 public class PlayNhacActivity extends AppCompatActivity {
 
@@ -25,26 +38,229 @@ public class PlayNhacActivity extends AppCompatActivity {
     SeekBar skttime;
     ImageButton imgplay, imgrepeat, imgnext, imgpre, imgradom;
     ViewPager viewpagerplaynhac;
+    public static ArrayList<BaiHat> mangbaihat = new ArrayList<>();
+    public static ViewPagerPlaylistnhac adapternhac;
+    Fragment_Dianhac fragment_dianhac;
+    Fragment_playdanhsachcacbaihat fragment_playdanhsachcacbaihat;
+    MediaPlayer mediaPlayer;
 
+    //
+    int position =0;// vi tri cac ca khuc, de next, pre
+    boolean repeat= false;
+    boolean checkrandom= false;
+    boolean next= false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_nhac);
+        StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        GetDataFromIntent();
         init();
+        eventClick();
+    }
+
+    private void eventClick() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (adapternhac.getItem(1)!=null){
+                    if(mangbaihat.size()>0){
+                        fragment_dianhac.PlayNhac(mangbaihat.get(0).getHinhbaihat());
+                        handler.removeCallbacks(this);
+                    }else{
+                        handler.postDelayed(this, 300); //0.3s
+                    }
+                }
+            }
+        }, 500);
+        imgplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()){
+                   mediaPlayer.pause();
+                   imgplay.setImageResource(R.drawable.iconplay);
+                }else{
+                    mediaPlayer.start();
+                    imgplay.setImageResource(R.drawable.iconpause);
+                }
+            }
+        });
+
+        imgrepeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(repeat ==false){
+                    if(checkrandom==true){
+                        checkrandom=false;
+                        imgrepeat.setImageResource(R.drawable.iconsyned);
+                        imgradom.setImageResource(R.drawable.iconsuffle);
+                    }
+                    imgrepeat.setImageResource(R.drawable.iconsyned);
+                    repeat = true;
+                }else{
+                    imgrepeat.setImageResource(R.drawable.iconrepeat);
+                    repeat = false;
+                }
+            }
+        });
+        imgradom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkrandom ==false){
+                    if(repeat==true){
+                        repeat=false;
+                        imgrepeat.setImageResource(R.drawable.iconrepeat);
+                        imgradom.setImageResource(R.drawable.iconshuffled);
+                    }
+                    imgradom.setImageResource(R.drawable.iconshuffled);
+                    checkrandom = true;
+                }else{
+                    imgradom.setImageResource(R.drawable.iconsuffle);
+                    checkrandom = false;
+                }
+            }
+        });
+
+        //seekbar su kien
+        skttime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+
+        //btn
+        imgnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mangbaihat.size()>0){
+                    if(mediaPlayer.isPlaying() || mediaPlayer!=null){
+                        mediaPlayer.stop();
+                        mediaPlayer.release();
+                        mediaPlayer =null;
+                    }
+                    if(position<(mangbaihat.size())){
+                        imgplay.setImageResource(R.drawable.iconpause);
+                        position++;
+                        if(repeat==true){
+                            if(position==0){
+                                position= mangbaihat.size();
+
+                            }
+                            position-=1;
+                        }
+                        if(checkrandom==true){
+                            Random random = new Random();
+                            int index= random.nextInt(mangbaihat.size());
+                            if(index == position){
+                                position=index-1;
+                            }
+                            position = index;
+                        }
+                        if(position>(mangbaihat.size()-1)){
+                            position =0;
+
+                        }
+                        new PlayMp3().execute(mangbaihat.get(position).getLinkbaihat());
+                        fragment_dianhac.PlayNhac(mangbaihat.get(position).getHinhbaihat());
+                        getSupportActionBar().setTitle(mangbaihat.get(position).getTenbaihat());
+                    }
+                }
+                // set su kien nguoi dung click nhieu qua
+                imgpre.setClickable(false);
+                imgnext.setClickable(false);
+                Handler handler1= new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imgpre.setClickable(true);
+                        imgnext.setClickable(true);
+                    }
+                },3000);
+            }
+        });
+
+
+        imgpre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mangbaihat.size()>0){
+                    if(mediaPlayer.isPlaying() || mediaPlayer!=null){
+                        mediaPlayer.stop();
+                        mediaPlayer.release();
+                        mediaPlayer =null;
+                    }
+                    if(position<(mangbaihat.size())){
+                        imgplay.setImageResource(R.drawable.iconpause);
+                        position--;
+                        if(position<0){
+                            position= mangbaihat.size()-1;
+                        }
+                        if(repeat==true){
+                            position=1;
+                        }
+                        if(checkrandom==true){
+                            Random random = new Random();
+                            int index= random.nextInt(mangbaihat.size());
+                            if(index == position){
+                                position=index-1;
+                            }
+                            position = index;
+                        }
+
+                        new PlayMp3().execute(mangbaihat.get(position).getLinkbaihat());
+                        fragment_dianhac.PlayNhac(mangbaihat.get(position).getHinhbaihat());
+                        getSupportActionBar().setTitle(mangbaihat.get(position).getTenbaihat());
+                    }
+                }
+                // set su kien nguoi dung click nhieu qua
+                imgpre.setClickable(false);
+                imgnext.setClickable(false);
+                Handler handler1= new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        imgpre.setClickable(true);
+                        imgnext.setClickable(true);
+                    }
+                },3000);
+            }
+        });
+
+
+
+    }
+
+    private void GetDataFromIntent() {
         Intent intent = getIntent();
-        if(intent.hasExtra("cakhuc")){
-            BaiHat baihat = intent.getParcelableExtra("cakhuc");
-            //Toast.makeText(this, baihat.getTenbaihat(), Toast.LENGTH_SHORT).show();
+        mangbaihat.clear();
+        if(intent !=null){
+            if(intent.hasExtra("cakhuc")){
+                BaiHat baihat = intent.getParcelableExtra("cakhuc");
+                //Toast.makeText(this, baihat.getTenbaihat(), Toast.LENGTH_SHORT).show();
+                mangbaihat.add(baihat);
+            }
 
-        }
-
-        if(intent.hasExtra("cacbaihat")){
-            ArrayList<BaiHat> mangbaihat =intent.getParcelableArrayListExtra("cacbaihat");
-            for(int i=0; i<mangbaihat.size();i++ ){
-                Log.d("BBB", mangbaihat.get(i).getTenbaihat());
-
+            if(intent.hasExtra("cacbaihat")){
+                ArrayList<BaiHat> baihatarraylist =intent.getParcelableArrayListExtra("cacbaihat");
+                mangbaihat = baihatarraylist;
+            /*for(int i=0; i<mangbaihat.size();i++ ){
+                //Log.d("BBB", mangbaihat.get(i).getTenbaihat());
+            }*/
             }
         }
     }
@@ -66,11 +282,73 @@ public class PlayNhacActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+                mediaPlayer.stop(); //khi dang hat thi dung phat, tat
+                mangbaihat.clear(); // xoa danh sach phat
             }
         });
 
         toolbarplaynhac.setTitleTextColor(Color.WHITE);
+        fragment_dianhac = new Fragment_Dianhac();
+        fragment_playdanhsachcacbaihat = new Fragment_playdanhsachcacbaihat();
+        adapternhac = new ViewPagerPlaylistnhac(getSupportFragmentManager());
+        adapternhac.Addfragnent(fragment_playdanhsachcacbaihat);
+        adapternhac.Addfragnent(fragment_dianhac);
+        viewpagerplaynhac.setAdapter(adapternhac);
 
+       fragment_dianhac= (Fragment_Dianhac) adapternhac.getItem(1);
 
+        if(mangbaihat.size()>0){
+            getSupportActionBar().setTitle(mangbaihat.get(0).getTenbaihat());
+            new PlayMp3().execute(mangbaihat.get(0).getLinkbaihat());
+            imgplay.setImageResource(R.drawable.iconpause);
+        }
     }
+
+
+
+     class PlayMp3 extends AsyncTask<String, Void, String> {
+
+
+         @Override
+         protected String doInBackground(String... strings) {
+
+             return strings[0];
+         }
+
+         @Override
+         protected void onPostExecute(String baihat) {
+             super.onPostExecute(baihat);
+
+             try {
+             mediaPlayer = new MediaPlayer();
+             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+             // thread online
+             mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                 // ham nay dung de khi getfile tren server lau thi dung thread nay lai vaf reset
+                 @Override
+                 public void onCompletion(MediaPlayer mp) {
+                     mediaPlayer.stop();
+                     mediaPlayer.reset();
+                 }
+             });
+
+                 mediaPlayer.setDataSource(baihat);
+                 mediaPlayer.prepare();
+
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+             mediaPlayer.start();
+             TimeSong();
+         }
+
+         // tao thu tu phat mot danh sach ca khuc nao do
+     }
+
+    private void TimeSong() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm:ss");
+        txtTotaltimesong.setText(simpleDateFormat.format(mediaPlayer.getDuration()));
+        skttime.setMax(mediaPlayer.getDuration());
+    }
+
 }
---
+++ app/src/main/java/com/example/zingmp3/Fragment/Fragment_Dianhac.java
@@ -0,0 +1,43 @@
+package com.example.zingmp3.Fragment;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.zingmp3.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Fragment_Dianhac extends Fragment {
+
+    View view;
+    CircleImageView circleImageView;
+    ObjectAnimator objectAnimator;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_dianhac, container, false);
+        circleImageView= view.findViewById(R.id.imageviewcicrle);
+        objectAnimator = objectAnimator.ofFloat(circleImageView, "rotation",0f, 360f);
+        objectAnimator.setDuration(100000);
+        objectAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        objectAnimator.setRepeatMode(ValueAnimator.RESTART);
+        objectAnimator.setInterpolator(new LinearInterpolator());
+        return view;
+    }
+
+    public void PlayNhac(String hinhanh){
+
+        //circleImageView.setImageURI(hinhanh);
+    }
+}
